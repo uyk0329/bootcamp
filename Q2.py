9232aa689dd9cfbcddfd3551f7e9dea97d8468e9
ruudska6 <feels19@naver.com>
2024-08-30 15:26:29 +0900

    error fixed

--- Q2.py
+++ Q2.py
@@ -1,19 +1,17 @@
-arrayA = map(int,input().split(","))
-arrayB = map(int,input().split(","))
+arrayA = list(map(int,input().split(",")))
+arrayB = list(map(int,input().split(",")))
 
 def remove_dupl(array):
     return set(array)
 
 def union(array1,array2):
-    merge_array = array1 + array2
-    union_array = merge_array.union()
+    union_array = array1.union(array2)
     return union_array
 
-def intersection(array1, array2):
-    merge_array = array1 + array2
-    inter_array = merge_array.intersection()
+def inter(array1, array2):
+    inter_array = array1.intersection(array2)
     return inter_array
 
 if __name__ == '__main__':
-    print(intersection(arrayA, arrayB))
+    print(list(inter(set(arrayA), set(arrayB))))
         
